83e5571671a45ecc0e73ef546acb090c18bc45a5
FalcasantosKrizelleMae <bg201803214@wmsu.edu.ph>
2021-12-15 04:19:05 +0800

    db

--- Config/db_connection.js
+++ Config/db_connection.js
@@ -6,10 +6,11 @@ dotenv.config({ path: './.env' });
 
 //Connect to Database
 var db = mysql.createConnection({
-   host: process.env.DATABASE_HOST,
-   user: process.env.DATABASE_USER,
-   password: process.env.DATABASE_PASSWORD,
-   database: process.env.DATABASE,
+   host: 'us-cdbr-east-05.cleardb.net',
+   user: 'b81b197e25d54a',
+   password: 'bb7f0480',
+   database: 'heroku_4c7d942fdcea2e2',
+   port: 3306,
 });
 
 db.connect((err) => {
